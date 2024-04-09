ddbdbb51dc9f2fa6e725af7a0fa68a7b65945389
IngoMiranda <ingolima26@gmail.com>
2024-04-09 06:50:49 -0300

    Evento e função de adicionar a lista os novos valores

--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
-const filterSelect = document.querySelector("#result-filter");
+const todoForm = document.querySelector('#todo-form');
+const inTodo = document.querySelector('#inTodo');
+const editForm = document.querySelector('#edit-form');
+const editInput = document.querySelector('#editInput');
+const cancelEdit = document.querySelector('#cancel-edit-btn');
+const todoList = document.querySelector('#todo-list');
 
-filterSelect.classList.add("none");
+// funções 
+const saveTodo = (text)=>{
+
+    const todo = document.createElement('div');
+    todo.classList.add('todo');
+
+    const btnCheck = document.createElement('button');
+    btnCheck.classList.add('finish-todo');
+    btnCheck.innerHTML = '<i class="fi fi-br-check"></i>';
+    todo.appendChild(btnCheck);
+
+    const todoTitle = document.createElement('h3');
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const btnEdit = document.createElement('button');
+    btnEdit.classList.add('finish-todo');
+    btnEdit.innerHTML = '<i class="fi fi-br-edit"></i>';
+    todo.appendChild(btnEdit);
+    
+    const btnCancel = document.createElement('button');
+    btnCancel.classList.add('finish-todo');
+    btnCancel.innerHTML = '<i class="fi fi-br-cross-small"></i>';
+    todo.appendChild(btnCancel);
+
+    todoList.append(todo);
+
+}
+
+function clearIntput(){
+    inTodo.value = "";
+};
+
+
+// Eventos
+
+todoForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const inputValue = inTodo.value;
+    
+    if(inputValue){
+        saveTodo(inputValue);
+    }
+    clearIntput(inTodo.value);
+});
